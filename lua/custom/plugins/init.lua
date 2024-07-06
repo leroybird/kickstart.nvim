1e47ed606e8d0364add6aec52cd111ce62716e48
Leroy Bird <leroybird1@gmail.com>
2024-07-06 16:46:15 +1200

    vscode nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,25 @@
 -- See the kickstart.nvim README for more information
 return {
   'github/copilot.vim',
+  -- image nvim options table. Pass to `require('image').setup`
+  {
+    '3rd/image.nvim',
+    config = function()
+      require('image').setup {
+        backend = 'kitty', -- Kitty will provide the best experience, but you need a compatible terminal
+        integrations = {}, -- do whatever you want with image.nvim's integrations
+        max_width = 100, -- tweak to preference
+        max_height = 12, -- ^
+        max_height_window_percentage = math.huge, -- this is necessary for a good experience
+        max_width_window_percentage = math.huge,
+        window_overlap_clear_enabled = true,
+        window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', '' },
+      }
+    end,
+  },
+}
+
+--[[
   'kevinhwang91/promise-async',
   'kevinhwang91/nvim-ufo',
   {
@@ -152,4 +171,4 @@ return {
   },
   'rcarriga/nvim-notify', -- optional
   'stevearc/dressing.nvim', -- optional, UI for :JupyniumKernelSelect
-}
+  ]]
